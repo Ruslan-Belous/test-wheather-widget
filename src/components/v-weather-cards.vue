5081514ec321dae31e0e0e50c7baa8e632a20a3f
Ruslan.Belous <r.bilous@viseven.com>
2022-07-17 01:05:56 +0300

    add(v-weather-cards):added new component

---
+++ src/components/v-weather-cards.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="weather-cards">
+		<v-weather-item v-for="card in places" :key="card.id" :wheather_item_data="card" />
+	</div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import vWeatherItem from './v-weather-item.vue';
+export default {
+	components: { vWeatherItem },
+	name: 'v-weather-cards',
+	computed: {
+		...mapState(['places']),
+		card() {
+			console.log(this.places);
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped></style>
