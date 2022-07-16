0a6f84b37ecafd054e37c8781412288b99686fe4
Ruslan.Belous <r.bilous@viseven.com>
2022-07-17 01:04:12 +0300

    add(v-main-container):added new component

--- src/components/v-main-container.vue
+++ src/components/v-main-container.vue
@@ -1,9 +1,16 @@
 <template>
-	<div class="main-container container">wheather app</div>
+	<main class="main-container container">
+		<v-weather-item />
+		<v-search-form />
+	</main>
 </template>
 
 <script>
+import VSearchForm from './v-search-form.vue';
+import VWeatherItem from './v-weather-item.vue';
+
 export default {
+	components: { VSearchForm, VWeatherItem },
 	name: 'v-main-container'
 };
 </script>
