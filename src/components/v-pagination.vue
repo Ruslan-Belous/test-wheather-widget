46ac8f23c8af4c5c752884e3e78ebe21d61e07b8
Ruslan.Belous <r.bilous@viseven.com>
2022-07-17 22:46:15 +0300

    update(main):update main

---
+++ src/components/v-pagination.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="pagination">
+		<div class="page" v-for="page in pages" :key="page">
+			{{ page }}
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'v-pagination',
+	data: () => ({
+		userPerPage: 5
+	}),
+	props: {
+		arrData: {
+			type: Array,
+			default() {
+				return [];
+			}
+		}
+	},
+	computed: {
+		pages() {
+			return Math.ceil(this.arrData.length / 5);
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped></style>
