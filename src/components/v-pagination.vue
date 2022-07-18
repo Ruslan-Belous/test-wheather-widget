ff850ba7f96f1c15d1ec0595298ad2bb40362b44
Ruslan.Belous <r.bilous@viseven.com>
2022-07-18 17:38:32 +0300

    add(v-pagination):added component

--- src/components/v-pagination.vue
+++ src/components/v-pagination.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="pagination">
-		<div class="page" v-for="page in pages" :key="page">
+		<div class="pagination__page" v-for="(page, i) in pages" :key="i" @click="pageClick(page)">
 			{{ page }}
 		</div>
 	</div>
@@ -10,7 +10,8 @@
 export default {
 	name: 'v-pagination',
 	data: () => ({
-		userPerPage: 5
+		pageNum: 1,
+		placePerPage: 2
 	}),
 	props: {
 		arrData: {
@@ -21,11 +22,39 @@ export default {
 		}
 	},
 	computed: {
+		isPaginationPageVisible() {
+			return this.arrData.length > 5;
+		},
 		pages() {
-			return Math.ceil(this.arrData.length / 5);
+			return Math.ceil(this.arrData.length / 2);
+		}
+	},
+	methods: {
+		pageClick(page) {
+			this.pageNum = page;
+			this.$emit('page-changed', page);
 		}
 	}
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.pagination {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	margin: 20px 0;
+	&__page {
+		padding: 6px 10px;
+		margin-right: 10px;
+		border: 1px solid #eee;
+		color: #fff;
+		border-radius: 8px;
+		transition: 0.3s;
+		cursor: pointer;
+	}
+	&__page:hover {
+		background-color: #102776;
+	}
+}
+</style>
