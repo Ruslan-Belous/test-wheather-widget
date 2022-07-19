ec5e1e5adf98596e361b1d42aa306c7a7769c8be
Ruslan.Belous <r.bilous@viseven.com>
2022-07-20 00:43:47 +0300

    deploy

---
+++ precache-manifest.70c8e67f7117a29200124aa45697cca8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfab693ef92c8f848002",
+    "url": "/test-wheather-widget/css/app.72eb4663.css"
+  },
+  {
+    "revision": "34b9e26a043b8fdc54b5",
+    "url": "/test-wheather-widget/css/chunk-vendors.34378d7f.css"
+  },
+  {
+    "revision": "39bfea5e86f5f41c9d2896dbbed6791b",
+    "url": "/test-wheather-widget/fonts/MaterialIcons-Regular.39bfea5e.woff"
+  },
+  {
+    "revision": "ae8b1248595e70a828b880b9c56963da",
+    "url": "/test-wheather-widget/fonts/MaterialIcons-Regular.ae8b1248.eot"
+  },
+  {
+    "revision": "b073f5972d9c4cc1b8ae8e071e441376",
+    "url": "/test-wheather-widget/fonts/MaterialIcons-Regular.b073f597.woff2"
+  },
+  {
+    "revision": "b9968c1adf542baec8d2b298d3ff437c",
+    "url": "/test-wheather-widget/fonts/MaterialIcons-Regular.b9968c1a.ttf"
+  },
+  {
+    "revision": "f03d0404172eb0529116cb3c9fe12c1f",
+    "url": "/test-wheather-widget/img/humidity.f03d0404.png"
+  },
+  {
+    "revision": "7780fa1bd56df05bddf0bb9404581336",
+    "url": "/test-wheather-widget/img/pressure.7780fa1b.png"
+  },
+  {
+    "revision": "882e1a432bd1ced107bbabbb67ee2143",
+    "url": "/test-wheather-widget/img/wethaer-bg1.882e1a43.jpeg"
+  },
+  {
+    "revision": "f10d6e9e3154b59123705485e982cca4",
+    "url": "/test-wheather-widget/img/wethaer-bg2.f10d6e9e.jpeg"
+  },
+  {
+    "revision": "e6d2b0f892aa0fb62fbb3936781a73fb",
+    "url": "/test-wheather-widget/img/wind.e6d2b0f8.png"
+  },
+  {
+    "revision": "725f9c2c018580a278d7004275444f70",
+    "url": "/test-wheather-widget/index.html"
+  },
+  {
+    "revision": "bfab693ef92c8f848002",
+    "url": "/test-wheather-widget/js/app.39f95193.js"
+  },
+  {
+    "revision": "34b9e26a043b8fdc54b5",
+    "url": "/test-wheather-widget/js/chunk-vendors.0ebc405b.js"
+  },
+  {
+    "revision": "bbc179deaa92ab53362aafac5f13a946",
+    "url": "/test-wheather-widget/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/test-wheather-widget/robots.txt"
+  }
+]);
